c34d2f2beb5a7bbf3a8f45fa5aa1575e31c2bc02
alvaro-star <vargasalvaro248@gmail.com>
2022-06-16 20:57:20 -0400

    Version 4.3 - formMulta modificao - Nao satisfeito

--- app/Views/forms/formMulta.php
+++ app/Views/forms/formMulta.php
@@ -9,6 +9,7 @@
                     <?= $dados['erro']['ano'] ?>
                 </div>
             </div>
+
             <div class="form-floating">
                 <input value="<?= $dados['dado']['cidade'] ?>" name="cidade" class="form-control <?= $dados['erro']['cidade'] ? 'is-invalid' : '' ?>" placeholder="Cidade">
                 <label for="floatingPassword">Cidade</label>
@@ -18,20 +19,18 @@
             </div>
 
             <!-- Funcao do php-->
-            
-            <label>Carro da Multa</label>
-            <select name = 'idtb_carro' class = 'selectMulta form-select' aria-label='Default select example'>
-                <option value='' > Escolha um carro...</option>
+
+            <select name='idtb_carro' class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <option value=''> Escolha um carro...</option>
                 <?php foreach ($dados['carros'] as $carro) : ?>
-                    <option value='<?=$carro->idtb_carro?>' > <?=$carro->placa;?>  </option>
+                    <option <?=($carro->idtb_carro == $dados['dado']['idtb_carro']) ? 'selected': ''?> value='<?= $carro->idtb_carro ?>'> <?= $carro->placa; ?> </option>
                 <?php endforeach ?>
             </select>
 
-            <label>Infração da Multa</label>
-            <select name = 'idtb_infracao' class = 'selectMulta form-select' aria-label='Default select example'>
-            <option value=''> Escolha uma infracao...</option>
+            <select name='idtb_infracao' class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                <option value=''> Escolha uma infracao...</option>
                 <?php foreach ($dados['infracoes'] as $infracao) : ?>
-                    <option value = '<?=$infracao->idtb_infracao?>' > <?=$infracao->descricao?></option>
+                    <option <?=($infracao->idtb_infracao == $dados['dado']['idtb_infracao']) ? 'selected': ''?> value='<?= $infracao->idtb_infracao ?>'> <?= $infracao->descricao ?></option>
                 <?php endforeach ?>
             </select>
 
