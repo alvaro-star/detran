0765dfcf27e6dd65a46a4422553553b1fcfae24e
ALVARO VARGAS ALVAREZ  TecInf <07884569116@laboratorio.ifms.br>
2022-06-28 13:20:22 -0400

    Version 7.1 - Configuracao do formulario cadastrar e login fo realizado

--- app/Views/usuarios/cadastrar.php
+++ app/Views/usuarios/cadastrar.php
@@ -1,40 +1,53 @@
-<div class='container div-principal'>
-
-    <form class="container text-center" method="POST" action="<?= URL ?>/usuarios/cadastrar">
-        <h1>Cadastre-se</h1>
-        <div class="form-floating">
-            <input type="text" value="<?= $dados['nome'] ?>" name="nome" class="form-control <?= $dados['erro']['nome'] ? 'is-invalid' : 'input' ?>" id="floatingName" placeholder="Nome">
-            <label for="floatingName">Nome</label>
-            <div class="invalid-feedback">
-                <?= $dados['erro']['nome'] ?>
-            </div>
+<div class="col-xl-4 col-md-6 mx-auto p-5 text-center">
+    <div class="card">
+        <div class="card-header bg-secondary text-white">
+            Cadastro
         </div>
+        <div class="card-body">
+            <?= Sessao::mensagem('usuario') ?>
+            <p class="card-text"><small class="text-muted">Informe seus dados</small></p>
 
-        <div class="form-floating">
-            <input type="text" value="<?= $dados['email'] ?>" name="email" class="form-control <?= $dados['erro']['email'] ? 'is-invalid' : 'input' ?>" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Endereço de email</label>
-            <div class="invalid-feedback">
-                <?= $dados['erro']['email'] ?>
-            </div>
+            <form name="login" method="POST" action="<?= URL ?>/usuarios/cadastrar" class="mt-4">
+
+                <div class="form-group">
+                    <label for="nome"><sup class="text-danger">*</sup> E-mail <sup class="text-danger">*</sup></label>
+                    <input type="text" name="nome" id="nome" value="<?= $dados['nome'] ?>" class="form-control <?= $dados['erro']['nome'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $dados['erro']['nome'] ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="email"><sup class="text-danger">*</sup> E-mail <sup class="text-danger">*</sup></label>
+                    <input type="text" name="email" id="email" value="<?= $dados['email'] ?>" class="form-control <?= $dados['erro']['email'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $dados['erro']['email'] ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="senha"><sup class="text-danger">*</sup> Senha <sup class="text-danger">*</sup></label>
+                    <input type="password" name="senha" id="senha" value="<?= $dados['senha'] ?>" class="form-control  <?= $dados['erro']['senha'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $dados['erro']['senha'] ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="confirmar_senha"><sup class="text-danger">*</sup> Confirmar Senha <sup class="text-danger">*</sup></label>
+                    <input type="password" name="confirmar_senha" id="confirmar_senha" value="<?= $dados['confirmar_senha'] ?>" class="form-control  <?= $dados['erro']['confirmar_senha'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $dados['erro']['confirmar_senha'] ?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="">
+                        <input type="submit" value="Login" class="btn btn-info btn-block">
+                    </div>
+                </div>
+
+            </form>
         </div>
-
-        <div class="form-floating">
-            <input type="password" value="<?= $dados['senha'] ?>" name="senha" class="form-control <?= $dados['erro']['senha'] ? 'is-invalid' : 'input' ?>" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Senha</label>
-            <div class="invalid-feedback">
-                <?= $dados['erro']['senha'] ?>
-            </div>
-        </div>
-
-        <div class="form-floating">
-            <input type="password" value="<?= $dados['confirmar_senha'] ?>" name="confirmar_senha" class="form-control <?= $dados['erro']['confirmar_senha'] ? 'is-invalid' : 'input' ?>" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Confirmar Senha</label>
-            <div class="invalid-feedback">
-                <?= $dados['erro']['confirmar_senha'] ?>
-            </div>
-        </div>
-
-        <button class="btn btn-primary" type="submit">Sign in</button>
-    </form>
-
+    </div>
 </div>
--- app/Views/usuarios/login.php
+++ app/Views/usuarios/login.php
@@ -1,23 +1,37 @@
-<div class="container div-principal">
-
-    <?= Sessao::mensagem('usuario') ?>
-    <form class="form text-center" method="POST" action="<?= URL ?>/usuarios/login">
-        <h1 class="h3 mb-3 fw-normal">Informe os seus dados</h1>
-        <div class="form-floating">
-            <input type="text" value="<?= $dados['email'] ?>" name="email" class="form-control <?= $dados['erro']['email'] ? 'is-invalid' : 'input' ?>" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Endereço de email</label>
-            <div class="invalid-feedback">
-                <?= $dados['erro']['email'] ?>
-            </div>
+<div class="col-xl-4 col-md-6 mx-auto p-5 text-center">
+    <div class="card">
+        <div class="card-header bg-secondary text-white">
+            Login
         </div>
+        <div class="card-body">
+            <?= Sessao::mensagem('usuario') ?>
+            <p class="card-text"><small class="text-muted">Informe seus dados</small></p>
 
-        <div class="form-floating">
-            <input type="password" value="<?= $dados['senha'] ?>" name="senha" class="form-control <?= $dados['erro']['senha'] ? 'is-invalid' : 'input' ?>" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Senha</label>
-            <div class="invalid-feedback">
-                <?= $dados['erro']['senha'] ?>
-            </div>
+            <form name="login" method="POST" action="<?= URL ?>/usuarios/login" class="mt-4">
+
+                <div class="form-group">
+                    <label for="email"><sup class="text-danger">*</sup> E-mail <sup class="text-danger">*</sup></label>
+                    <input type="text" name="email" id="email" value="<?= $dados['email'] ?>" class="form-control <?= $dados['erro']['email'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $dados['erro']['email'] ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="senha"><sup class="text-danger">*</sup> Senha <sup class="text-danger">*</sup></label>
+                    <input type="password" name="senha" id="senha" value="<?= $dados['senha'] ?>" class="form-control  <?= $dados['erro']['senha'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $dados['erro']['senha'] ?>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="">
+                        <input type="submit" value="Login" class="btn btn-info btn-block">
+                    </div>
+                </div>
+
+            </form>
         </div>
-        <button class="btn btn-primary" type="submit">Sign in</button>
-    </form>
+    </div>
 </div>
+<!--col-md-6--->
