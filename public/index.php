f0961066559eb80f936db2f91e47f40adf722dc6
alvaro-star <vargasalvaro248@gmail.com>
2022-07-03 13:11:47 -0400

    Versao 8.2 - Troca do Include

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/detran.png"/>
     <title><?= APP_NAME?></title>
-    <?php include(PUBLICO.'\css\bootstrapCss.php'); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../public/css/container.css" rel="stylesheet">
 
 </head>
