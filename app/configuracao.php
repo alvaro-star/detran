2ff02d49e971f7971422c351619d96dd9523bb04
alvaro-star <vargasalvaro248@gmail.com>
2022-07-03 14:25:23 -0400

    Realizando testes

--- app/configuracao.php
+++ app/configuracao.php
@@ -2,7 +2,7 @@
 //echo dirname(dirname(__FILE__));//aula 11
     define('APP', dirname(__FILE__)); //Aula 11
     define('PUBLICO', dirname(dirname(__FILE__)) . "/public");
-    define('URL', 'detran-ava.herokuapp.com');
+    define('URL', 'https://detran-ava.herokuapp.com');
     define('APP_NAME', 'Detran-MS');
     const APP_VERSAO = '7.0.0';
 ?>
